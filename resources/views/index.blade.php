4784a24952c7151e8327ec79a84a24c0757b1da5
jacquesTshingambu <jacquestshingambu27@gmail.com>
2022-02-09 10:48:58 +0100

    intergrer git dans mon projet

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+@section('meta')
+    <meta charset="UTF-8">
+    <meta name="description" content="Tutoriel Laravel 8 gratuit">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="John Doe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@endsection
+
+@section('content')
+    <p>corps du contenu. </p>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        @yield('meta')
+    </head>
+    <body>
+        @section('sider')
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
