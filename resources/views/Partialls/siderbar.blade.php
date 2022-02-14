bad2bd8ee1980b85e19e5ee61914f98cef4a2b6b
jacquesTshingambu <jacquestshingambu27@gmail.com>
2022-02-14 15:19:05 +0100

    enregitrement de quelques fonctionnement

---
+++ resources/views/Partialls/siderbar.blade.php
@@ -0,0 +1,16 @@
+gi<div class="card my-4">
+    <h5 class="card-header">Catégories</h5>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                    @foreach($categories as $category)
+                        <li>
+                            <a href="http://localhost:8000/category/{{$category['slug']}}">{{$category['name']}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
